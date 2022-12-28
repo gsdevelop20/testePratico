f85732d5d20d0ef95d46ac238efb7cd368f018b9
Gabriel Santos <gabrildado08@gamil.com>
2022-12-28 11:11:26 -0300

    add

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ $( "#btn-menu" ).click(function() {
     if(n===0){
         $( "#hidder-menu" ).css("display","flex")
         n=1;
+        $( "#img-toggle").attr("src","./imagem/icons8-x-48 (1).png");
+          
     }else if(n===1){
         $( "#hidder-menu" ).css("display","none")
         n=0
+        $( "#img-toggle").attr("src","./imagem/icons8-menu-arredondado-50.png ");
+        
     }
   });
